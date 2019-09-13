2c719837fc3f73fc7754350655995af2b319f2d3
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-12 23:28:25 -0400

    4.2 completed: added animations to picture

--- BIP the guy/ViewController.swift
+++ BIP the guy/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //MARK: Functions
+    func animateImage(){
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our imageToPunch by 60 pixels
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+        
+    }
+    
+    //MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -25,7 +37,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imagedTapped(_ sender: UITapGestureRecognizer) {
-        print("hey! you just pressed the image")
+        animateImage()
     }
 }
 
